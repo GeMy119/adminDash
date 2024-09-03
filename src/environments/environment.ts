1ab56e4b6630a92b64dc011368f63801a7fe3a1f
GeMy119 <mahmoud.gamal1191998@gmail.com>
2024-09-03 19:57:13 +0300

    update link api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export interface Environment {
 }
 export const environment: Environment = {
   production: false,
-  apiUrl: "https://api.saudiservicess.com"
+  apiUrl: "https://api.suadiestalamsurvices.com"
 
 };
 
